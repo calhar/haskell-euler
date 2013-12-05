a8fa30b0407c110a12349445b153b30a00cf862b
Calum <non3xistent@gmail.com>
2013-12-05 03:50:22 +0000

    Forgot to use limit instead of 1000 in multiples function body\n\nAlso forgot to that [1..1000] includes 1000.

--- 01/main.hs
+++ 01/main.hs
@@ -1,5 +1,5 @@
 main = print ( multiples 1000 3 5 )
 
 multiples :: (Integral a) => a -> a -> a -> a
-multiples limit x y = sum [ z | z <- [1..1000], z `mod` x == 0 || z `mod` y == 0]
+multiples limit x y = sum [ z | z <- [1..limit-1], z `mod` x == 0 || z `mod` y == 0]
 
