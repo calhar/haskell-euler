97816acd339de47f966af8e93935eeb584d0d135
Calum <non3xistent@gmail.com>
2014-04-11 21:55:52 +0100

    tweak to euler 02 for true, right and proper fib beginning 0, 1

--- 02/main.hs
+++ 02/main.hs
@@ -4,8 +4,8 @@ evenFib :: (Ord a, Integral a) => a -> a
 evenFib limit = sum [ z | z <- fibLim [] limit, z `mod` 2 == 0]
 
 fibLim :: (Ord a, Integral a) => [a] -> a -> [a]
-fibLim [] limit = fibLim [1] limit
-fibLim [1] limit = fibLim [2, 1] limit
+fibLim [] limit = fibLim [0] limit
+fibLim [0] limit = fibLim [1, 0] limit
 fibLim [x,y] limit = fibLim [x+y,x,y] limit
 fibLim numbers@(x:y:_) limit = if x + y < limit then 
                                 (fibLim ((x+y):numbers) limit)
