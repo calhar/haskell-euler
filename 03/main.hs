f885a72e7cae6ff467df5951cd5c258c15cc8460
Calum <non3xistent@gmail.com>
2014-04-11 21:54:36 +0100

    project euler 03 comments

--- 03/main.hs
+++ 03/main.hs
@@ -3,10 +3,17 @@ import Debug.Trace
 main = print(primeFac 600851475143 [])
 
 primeFac 1 _ = [1]
+-- if we have no primes, highest possible prime is val
+-- should really do something to check if sqrt(val) < p
+-- so val is only remaining factor and prime
 primeFac val [] = primeFac val (primes val)
+-- if only element in list
 primeFac val [p] = if val `mod` p == 0
                    then [p]
                    else []
+-- check if p is factor of val
+-- if so div val p and check again
+-- else discard p
 primeFac val (p:xs) = if val `mod` p == 0
                       then p : primeFac (val `div` p) (p:xs)
                       else primeFac val xs
