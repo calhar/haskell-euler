26fe1435720d695ab2ce9176915ea637e3aeabad
Calum <non3xistent@gmail.com>
2014-04-30 23:44:32 +0100

    euler 14 minor improvements

--- 14/main.hs
+++ 14/main.hs
@@ -1,16 +1,19 @@
-main = print $ maximum $ zip (map length $ map collatz [1..999999]) [1..999999]
+import Data.List
+import Data.Ord
+main = print $ maximumBy (comparing snd) $ map (\x -> (x, (collatzLen x))) [500000..999999]
 
--- simple implementation
+-- slight improved implementation
 --
--- for each val [1..999999]
--- get collatz sequence and take length
+-- for each val [500000..999999] since any val in [1..499999] will have another
+-- potential value in [500000..999999]
 --
--- zip with [1..999999] to get collatz length and val in form (length, val)
--- take maximum
+-- get collatz length -- don't bother making list and taking length
 --
--- a better way to do it would be to store created lists as we go in linked
--- lists, but whatever
-collatz val
-  | val == 1 = [1]
-  | val `mod` 2 == 0 = val : collatz (val `div` 2)
-  | otherwise = val : collatz (3 * val + 1)
+-- return the maximum given by comparing the second item in the tuple
+--
+-- a better way to do it would be to store lengths as we go and when we reach a
+-- known value add and stop
+collatzLen val
+  | val == 1 = 1
+  | val `mod` 2 == 0 = 1 +  collatzLen (val `div` 2)
+  | otherwise = 1 +  collatzLen (3 * val + 1)
