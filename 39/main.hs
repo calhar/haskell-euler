3a51b0ecabef755f4407ed8a07db738c62195fbc
Calum <non3xistent@gmail.com>
2014-05-01 19:59:30 +0100

    euler 39

---
+++ 39/main.hs
@@ -0,0 +1,29 @@
+import Data.List
+import Data.Ord
+
+main = print $ maximumBy (comparing snd) $
+          zip [1..1000] $ map length $ map (pythagTriple) [1..1000]
+
+-- as in euler 09
+-- we can get pythag triples using this
+--
+-- although apparently i need to fix some things here
+-- because 13 has the triple 3,4,5 showing up for it
+-- and 120 has 24, 32, 40 showing up for some reason
+--
+-- Whatever, another filter so that sum of the triple == val works too
+pythagTriple val = 
+                 let factors = factors_naive (val `div` 2)
+                     abc_list = [[b - a, a, ((val `div` 2) `div` (b * a))] |
+                                 a <- factors, b <- factors, gtxlt2x a b]
+                 in nub $ filter (\t -> sum t == val) $ map nmk2abc abc_list
+
+nmk2abc (m:n:k:[]) = sort [k*((n^2) - (m^2)),
+                           (k * (2 * m * n)), 
+                           k*((n^2) + (m^2))]
+
+-- check that x < y < 2x
+gtxlt2x x y = (y > x) && (y < 2*x)
+
+-- naive factorisation
+factors_naive n = [i | i <- [1 .. n], n `mod` i == 0]
